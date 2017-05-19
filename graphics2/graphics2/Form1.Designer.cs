3598737a24766da3e8ffd23612c10aa31dd61f9e
adi5785905 <adi5785905@gmail.com>
2017-05-19 19:25:31 +0300

    no savwafGpkwgjvwes

--- graphics2/graphics2/Form1.Designer.cs
+++ graphics2/graphics2/Form1.Designer.cs
@@ -41,7 +41,6 @@
             this.radioButton8 = new System.Windows.Forms.RadioButton();
             this.radioButton7 = new System.Windows.Forms.RadioButton();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.button3 = new System.Windows.Forms.Button();
@@ -219,20 +218,9 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(35, 782);
-            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(203, 67);
-            this.button2.TabIndex = 10;
-            this.button2.Text = "Save";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(22, 1010);
+            this.textBox1.Location = new System.Drawing.Point(22, 889);
             this.textBox1.Margin = new System.Windows.Forms.Padding(8, 5, 8, 5);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
@@ -242,7 +230,7 @@
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(22, 1303);
+            this.comboBox1.Location = new System.Drawing.Point(22, 1179);
             this.comboBox1.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(231, 39);
@@ -252,7 +240,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(35, 912);
+            this.button3.Location = new System.Drawing.Point(34, 779);
             this.button3.Margin = new System.Windows.Forms.Padding(8, 5, 8, 5);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(203, 67);
@@ -269,7 +257,6 @@
             this.Controls.Add(this.button3);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.panel1);
@@ -298,7 +285,6 @@
         private System.Windows.Forms.RadioButton radioButton8;
         private System.Windows.Forms.RadioButton radioButton9;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.RadioButton radioButton10;
         private System.Windows.Forms.ComboBox comboBox1;
